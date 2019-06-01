aa239875f18c928d31ea775dfa1076c3ed2c18a5
GrigoryCode <grigorycode@gmail.com>
2019-06-01 10:06:51 +0300

    Add pages and route it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Item from './pages/Item';
+import Error from './pages/Error';
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>Hello</>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/catalog" component={Catalog} />
+        <Route exact path="/catalog/:slug" component={Item} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
